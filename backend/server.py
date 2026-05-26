62c2fa90ded51d9c7bc7b879002107cea019a56c
pvl2013004 <pvl2013004@example.com>
2026-05-26 01:41:43 +0000

    fix top level fastapi app

--- backend/server.py
+++ backend/server.py
@@ -19,10 +19,10 @@ import asyncio
 
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
-
-# MongoDB connection
 app = FastAPI()
 api_router = APIRouter()
+
+# MongoDB connection
 mongo_url = os.environ['MONGO_URL']
 client = AsyncIOMotorClient(mongo_url)
 db = client[os.environ['DB_NAME']]
